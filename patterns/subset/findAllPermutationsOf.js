9e23e20422d4561ba0e4a588d2e6001a04124a8d
essohanam <essohanam.alou@imsp-uac.org>
2024-01-15 04:35:12 +0000

    subset medium

--- patterns/subset/findAllPermutationsOf.js
+++ patterns/subset/findAllPermutationsOf.js
@@ -3,10 +3,33 @@
 /**
  * Given a set of distinct numbers,
  *  find all of its permutations.
- * @param {Array} list - set of distinct numbers
+ * @param {number[]} list - set of distinct numbers
  * @returns {Array} list - set of all permutations
  */
 export default function findAllPermutationsOf(list) {
   if (!Array.isArray(list)) throw new Error("list must be an array");
   const permutations = [];
+
+  /**
+   * closure to traverse list and add permutations
+   * @param {number[]} current - partial pemutation being built
+   */
+  function backtrack(current) {
+    if (current.length === list.length) {
+      permutations.push([...current]);
+      return;
+    }
+    for (const number of list) {
+      if (current.includes(number)) continue;
+      current.push(number);
+      backtrack(current);
+      current.pop();
+    }
+  }
+
+  backtrack([]);
+  return permutations;
 }
+
+console.log(findAllPermutationsOf([1, 3, 2]));
+// o(n!) time and o(n) space complexity. n is the legth of list
---
+++ patterns/subset/getCasesPermutations.js
@@ -0,0 +1,12 @@
+"use strict";
+/**
+ * Given a string, find all of its permutations preserving
+ * the character seqquence but changing case.
+ * @param {string} str -string
+ * @returns {string[]} result - list of character case permutated
+ *
+ */
+export default function getCasesPermutations(str) {
+  if (!str || typeof str !== "string") throw new Error("invalid input");
+  const result = [];
+}
